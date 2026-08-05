58501392be4b9f008057613d36c1406fcf66c32d
Abdellahsyani <syaniabdos@gmail.com>
2026-08-05 17:21:56 +0100

    setup packages

--- src/app.js
+++ src/app.js
@@ -1,13 +1,10 @@
-const express = require("express");
+import express from "express";
+
 const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello from my first patch of express");
-});
-
-app.put("/user", (req, res) => {
-  res.send("Got a put in the user end point");
 });
 
 app.listen(PORT, () => {
