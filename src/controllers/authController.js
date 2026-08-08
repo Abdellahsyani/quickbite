a938de8b087045c5ee3fc059a02c4e96921a4284
Abdellahsyani <syaniabdos@gmail.com>
2026-08-08 15:27:23 +0100

    implement register api

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,9 +2,45 @@ import bcrypt from "bcryptjs";
 import { prisma } from "../config/db.js";
 
 export const register = async (req, res) => {
-  const { email, name, password } = req.body;
-}
+  try {
+    const { email, name, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res
+        .status(404)
+        .json({ message: "Please provide name, email, password" });
+    }
+
+    const existingUser = prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      return res.status(400).json({ messgae: "User already exists" });
+    }
+    const salt = await bcrypt.getSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+      select {
+        id: true,
+          name: true,
+          email: true,
+          role: true,
+          ceatedAt: true,
+      },
+    });
+    return res.status(201).json({message:"User registred successfully", user});
+  } catch (error) {
+    return res.status(500).json({message:"Server error", error: error.message});
+  }
+};
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
-}
+};
