b55ad6184e886bc7452620942911c31e37fd8200
Abdellahsyani <syaniabdos@gmail.com>
2026-08-08 10:03:53 +0100

    register & login in progress

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,2 +1,10 @@
 import bcrypt from "bcryptjs";
 import { prisma } from "../config/db.js";
+
+export const register = async (req, res) => {
+  const { email, name, password } = req.body;
+}
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+}
