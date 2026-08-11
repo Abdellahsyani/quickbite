9e0773973f8ec97bc04e94b26e538f909815fc00
Abdellahsyani <syaniabdos@gmail.com>
2026-08-11 11:16:29 +0100

    add menu table to database

---
+++ src/controllers/menuController.js
@@ -0,0 +1,26 @@
+import { prisma } from "../config/db.js";
+
+export const createMenu = async (req, res) => {
+  try {
+    if (req.user.role !== "admin") {
+      return res
+        .status(403)
+        .json({ message: "Access denied. Admin rights required" });
+    }
+
+    const { name, description, price, category } = req.body;
+    if (!name || !price || !category) {
+      return res
+        .status(400)
+        .json({ message: "Name, Price,, and category are required" });
+    }
+    const newItem = await prisma.menuItem.create({
+      date: { name, description, price: paseFloat(price), category },
+    });
+    return res.status(201).json({ message: "Menu Item created" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Server Error", error: error.message });
+  }
+};
