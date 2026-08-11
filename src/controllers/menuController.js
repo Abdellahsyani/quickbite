cc5f847a0bd3ac8343426078f234817fd131a405
Abdellahsyani <syaniabdos@gmail.com>
2026-08-11 15:57:29 +0100

    add update and get menu end points

--- src/controllers/menuController.js
+++ src/controllers/menuController.js
@@ -35,3 +35,54 @@ export const getMenuItems = async (req, res) => {
       .json({ message: "Server error", error: error.message });
   }
 };
+
+export const getMenuItembyId = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const MenuItemId = await prisma.MenuItem.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    if (!MenuItemId) {
+      return res.status(404).json({ message: "Menu item not found" });
+    }
+    return res.status(200).json(MenuItemId);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Server error", error: error.message });
+  }
+};
+
+export const updateMenuItem = async (req, res) => {
+  try {
+    const { name, description, price, category, isAvailable } = req.body;
+    const { id } = req.params;
+
+    const itemId = parseInt(id, 10);
+    if (isNaN(itemId)) {
+      return res.status(400).json({ message: "Invalid menu item ID" });
+    }
+    const updateItem = await prisma.MenuItem.update({
+      where: { id: itemI },
+      data: {
+        ...(name && { name }),
+        ...(description !== undefined && { description }),
+        ...(price !== undefined && { price: parseFloat(price) }),
+        ...(category && { category }),
+        ...(isAvailable !== undefined && { isAvailable }),
+      },
+    });
+    return res
+      .status(200)
+      .json({ message: "Item updated successfully", item: updateItem });
+  } catch (error) {
+    if (error.code === "P2025") {
+      return res.status(404).json({ message: "Menu item not found" });
+    }
+    return res
+      .status(500)
+      .json({ message: "Server error", error: error.message });
+  }
+};
