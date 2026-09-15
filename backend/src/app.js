5be8f8eafcf2b9a876d203add5e8ad5a33db0607
Abdellahsyani <syaniabdos@gmail.com>
2026-09-15 12:09:23 +0100

    add cors and update port

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { config } from "dotenv";
+import cors from "cors";
 import { connectDB, disconnectDB } from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -10,7 +11,9 @@ config();
 connectDB();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
